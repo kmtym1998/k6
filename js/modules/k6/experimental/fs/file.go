5c5401f1db322df76329c04caa556f40c357b3ee
Théo Crevon <oleiade@users.noreply.github.com>
2023-08-18 16:18:11 +0200

    Apply suggestions from code review
    Co-authored-by: Oleg Bespalov <oleg.bespalov@grafana.com>

--- js/modules/k6/experimental/fs/file.go
+++ js/modules/k6/experimental/fs/file.go
@@ -6,7 +6,6 @@ import (
 
 // file is an abstraction for interacting with files.
 type file struct {
-	// name holds the name of the file, as presented to [Open].
 	path string
 
 	// data holds a pointer to the file's data
@@ -24,6 +23,6 @@ type FileInfo struct {
 	// Name holds the base name of the file.
 	Name string `json:"name"`
 
-	// Name holds the length in bytes of the file.
+	// Size holds the size of the file in bytes.
 	Size int `json:"size"`
 }
